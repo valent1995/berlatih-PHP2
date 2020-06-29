184476a89e0ee0cf268fd812452c0e41fc76ec92
valent1995 <valentinus.paramarta@gmail.com>
2020-06-29 17:46:13 +0700

    mengedit echo break

--- cari-mean.php
+++ cari-mean.php
@@ -13,5 +13,5 @@ echo "<br>".cari_mean([3, 5, 7, 5, 3]); // 5
 echo "<br>".cari_mean([6, 5, 4, 7, 3]); // 5
 echo "<br>".cari_mean([1, 3, 3]); // 2
 echo "<br>".cari_mean([7, 7, 7, 7, 7]); // 7
-
+echo "<br>";
 ?>
